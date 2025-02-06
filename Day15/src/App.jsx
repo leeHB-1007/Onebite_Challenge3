5aca4b1d2c69aa1ff766962e5211344f837dc7d7
이현빈 <lhb0107@naver.com>
2025-02-06 20:55:39 +0900

    day15

---
+++ Day15/src/App.jsx
@@ -0,0 +1,49 @@
+import "./App.css";
+import ContactEditor from "./components/ContactEditor";
+import ContactList from "./components/ContactList";
+import { useReducer } from "react";
+import { useRef } from "react";
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "CREATE":
+      return [action.data, ...state];
+    case "REMOVE":
+      return state.filter((it) => it.id !== action.targetId);
+    default:
+      return state;
+  }
+};
+function App() {
+  const [contacts, dispatch] = useReducer(reducer, []);
+  const idRef = useRef(0);
+  const onPlusContact = (name, contact) => {
+    dispatch({
+      type: "CREATE",
+      data: {
+        id: idRef.current++,
+        name,
+        contact,
+      },
+    });
+  };
+  const onRemoveContact = (targetId) => {
+    dispatch({
+      type: "REMOVE",
+      targetId,
+    });
+  };
+  return (
+    <div className="App">
+      <h2>Contact List</h2>
+      <section>
+        <ContactEditor onPlusContact={onPlusContact} />
+      </section>
+      <section>
+        <ContactList contacts={contacts} onRemoveContact={onRemoveContact} />
+      </section>
+    </div>
+  );
+}
+
+export default App;
