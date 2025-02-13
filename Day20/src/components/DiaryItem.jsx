4690b366357bcb23f62913dc357d88dd30dd2cc9
이현빈 <lhb0107@naver.com>
2025-02-13 11:33:43 +0900

    day20

---
+++ Day20/src/components/DiaryItem.jsx
@@ -0,0 +1,52 @@
+import { useNavigate } from "react-router-dom";
+import { getEmotionImage } from "../util/get-emotion-image";
+import Button from "./Button";
+import "./DiaryItem.css";
+import { useContext } from "react";
+import { DiaryDispatchContext } from "../App";
+
+const DiaryItem = ({ id, emotionId, createdDate, content }) => {
+  const { onDelete } = useContext(DiaryDispatchContext);
+
+  const nav = useNavigate();
+
+  const goDiaryPage = () => {
+    nav(`/diary/${id}`);
+  };
+
+  const goEditPage = () => {
+    nav(`/edit/${id}`);
+  };
+
+
+  const onClickDelete = () => {
+    onDelete(id);
+  };
+
+  return (
+    <div className="DiaryItem">
+      <div
+        onClick={goDiaryPage}
+        className={`img_section img_section_${emotionId}`}
+      >
+        <img src={getEmotionImage(emotionId)} />
+      </div>
+      <div onClick={goDiaryPage} className="info_section">
+        <div className="created_date">
+          {new Date(createdDate).toLocaleDateString()}
+        </div>
+        <div className="content">{content}</div>
+      </div>
+      <div className="button_section">
+        <Button onClick={goEditPage} text={"수정하기"} />
+        <Button
+          type={"NEGATIVE"}
+          onClick={onClickDelete}
+          text={"삭제하기"}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default DiaryItem;
